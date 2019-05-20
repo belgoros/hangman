f35ea079eaa74aa6a3d2d7f00aa3671a4c2f2687
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2019-05-20 17:32:57 +0200

    Implement Game.aaccept_move and already used state

--- test/game_test.exs
+++ test/game_test.exs
@@ -17,4 +17,18 @@ defmodule GameTest do
       assert { ^game, _ } = Game.make_move(game, "x")
     end
   end
+
+  test "first occurence of letter is not already used" do
+    game = Game.new_game()
+    { game, _tally } = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+  end
+
+  test "second occurence of letter is not already used" do
+    game = Game.new_game()
+    { game, _tally } = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+    { game, _tally } = Game.make_move(game, "x")
+    assert game.game_state == :already_used
+  end
 end
